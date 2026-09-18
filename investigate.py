40144c2a56754bbd484a89d25750da9d8e5c5b84
prabhatram <prabhatdreamz@gmail.com>
2026-09-18 15:04:12 +0800

    gex2

---
+++ investigate.py
@@ -0,0 +1,71 @@
+## Import the necessary modules
+
+## Import the function from the module parse_data
+
+
+## Build your prompt based on the description the user provides 
+## and the items that are available in the lost-and-found database.
+## The model must follow the rules listed in the README file
+## The function should return the system prompt and the user prompt.
+## You may need to use json.dumps() to convert the available_items list into a JSON string.
+
+def build_prompt(description, available_items):
+    pass
+    
+
+## Logic to ask Qwen for all the possible matches based on the system prompt and user prompt.
+## The function should return the response from Qwen.
+def ask_qwen(system_prompt, user_prompt):
+    pass
+
+
+## Logic to parse the response from Qwen and return the result. 
+## You may need to use json.loads() to convert the response string into a suitable Python data structure.
+def parse_response(response_text):
+    pass
+    
+
+
+## Logic to validate the result returned by Qwen.
+## It should check if the result is a dictionary, contains the keys "matches" and "confidence", and that the values are of the correct type.
+## If everything is correct, then it should check if the item IDs in the "matches" list are valid IDs .
+def validate_result(result, available_items):
+    pass
+
+
+## Logic to display the matches found by Qwen in a user-friendly format.
+## It should look something like this:
+""" 
+CAMPUS LOST-AND-FOUND ASSISTANT
+==================================================
+
+Describe the item you lost: I lost a black bag somewhere
+
+Searching for possible matches...
+
+MATCH RESULT
+--------------------------------------------------
+Confidence: MEDIUM
+
+Possible matches:
+
+ID: F101
+Item: backpack
+Color: black
+Location: Library 2nd floor
+Date found: 2026-09-15
+
+Result saved to output/match_result.json
+ """
+## If no matches are found, it should display a message indicating that no matches were found, along with the empty list
+def display_matches(result, available_items):
+    pass
+    
+
+## Control center for the entire program.
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ parse_data.py
@@ -0,0 +1,18 @@
+## Import the necessary modules
+
+## Logic for loading and reading from a JSON file. 
+## The function must return only the items
+def load_items(filename):
+    pass
+
+
+## Logic for getting only those items that are not yet claimed 
+## It should return only the items that are unclaimed
+def get_unclaimed_items(items):
+    pass
+    
+
+## Logic to save the result to a JSON file.
+## The function should create the directory if it does not exist and save the result in a JSON format.
+def save_result(result, filename):
+    pass
